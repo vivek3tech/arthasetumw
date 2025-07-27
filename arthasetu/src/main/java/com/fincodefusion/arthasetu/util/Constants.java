317e5c285c69632b175e8e7527e03e5ac5605d49
“vivek3tech” <vivek3tech@gmail.com>
2025-07-27 23:42:48 +0530

    Added system context

---
+++ arthasetu/src/main/java/com/fincodefusion/arthasetu/util/Constants.java
@@ -0,0 +1,11 @@
+package com.fincodefusion.arthasetu.util;
+
+public class Constants {
+
+   public static String SYSTEM_PROMPT = """
+        please answer in short, simple and crisp statements
+        You are a helpful assistant. make use of Bank Account tools if asked about transfer money and account balance check
+        always reply in user prompt's language, use INR for amount balance and transfer
+        please answer always in polite way , you can store user's information, for other unusual abusive questions a reply please ask finance related questions""";
+
+}
