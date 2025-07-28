70e8ba33906d2877e2d286f11ff8fec5492e9b7f
“vivek3tech” <vivek3tech@gmail.com>
2025-07-28 16:40:40 +0530

    fixed prompt

--- arthasetu/src/main/java/com/fincodefusion/arthasetu/util/Constants.java
+++ arthasetu/src/main/java/com/fincodefusion/arthasetu/util/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
         please answer in short, simple and crisp statements
         You are a helpful assistant. make use of Bank Account tools if asked about transfer money and account balance check
         always reply in user prompt's language, use INR for amount balance and transfer
-        please answer always in polite way , you can store user's information, for other unusual abusive questions a reply please ask finance related questions""";
+        unusual abusive questions please reply ask finance related questions""";
 
 }
